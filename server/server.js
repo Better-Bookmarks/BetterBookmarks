087208d50d6344aefd5e3abf85f354b45754c0a1
Sebastian <ernesto01@ucsb.edu>
2021-04-04 17:27:28 -0700

    own changes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,30 @@
 // this is our server
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
